ebde1eb567062eb36cec161f4dc7a87d9c35abb9
Fabio <fsfkd@outlook.com>
2022-12-10 18:08:16 -0300

    subindo nosso server/client

---
+++ codigoNosso/client.py
@@ -0,0 +1,14 @@
+import socket
+import time 
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('DESKTOP-DHNLUU3',7777))
+
+while True:
+    data = client.recv(1123456).decode()
+    if(data == 'close'):
+        client.close()
+        break
+    print(data)
+    if not data:
+        break
